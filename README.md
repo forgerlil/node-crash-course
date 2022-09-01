0c21ccbd9616201ce05bb4f4b6c984fd0dd64989
Lilian <lilianforger@gmail.com>
2022-09-01 17:40:48 +0200

    Step 5 - Splitting the code by controller functions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Changes:
 
-1. A middleware was added to our simple GET endpoint.
+1. With the help of the MVC pattern, we started to split our code into more manageable chunks!
+2. The two functions in the index.js are now declared in controller.js and exported.
+3. The functions in controller.js are now imported into index.js file and used to handle the requests.
--- index.js
+++ index.js
@@ -3,25 +3,16 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-// From here, understanding middlewares is a small step away. 
-// A middleware is nothing but a function that, instead of closing the cycle, performs some action and then hands over to the next function(middleware) in the chain!
-// There is no limit or restriction on the number of middlewares you can use in a chain: You just need to pass to the next function and not close the cycle.
-app.get('/', 
-  // It is quite important that, if your function is meant to be a middleware, that it has a third parameter, next. It will be a function that when called will
-  // stop the execution of the current middleware and hand over to the next one.
-  (req, res, next) => {
-    console.log('I am the middleware!');
-    // The request that you get is an object! So maybe you want to receive some data and add a property to your request object before you pass over to the next function, like so:
-    req.someProperty = 'I was added here by the middleware!';
-    // Remember: if you don't call next at any point of the middleware, your next function in line will never run (and your client will be stuck waiting for a response).
-    if (req.someProperty) next();
-  },
-  (req, res) => {
-    // Let's see what our middleware handed over to us:
-    console.log(req.someProperty);
-    // And let's send a response to the client:
-    res.send('The middleware added this property to the request object: ' + req.someProperty);
-  }
-);
+// Now, this small example is already getting cumbersome, what if we had tons of routes, each with a request? Time to make use of the MVC pattern (or at least part of it)!
+// The MVC pattern (stands for Model-View-Controller) is a way of structuring your code to make it more organized and easier to maintain. 
+// It's a very common pattern in backend development.
+// For that we move all of our functions into a controller file, and then import it into our index.js file.
+// Checkout the controller.js file too!
+
+// We import the two functions here (remember to add the extension of the file at the end!)
+const {myMiddleware, myRootRouteHandler} = require('./controller.js');
+
+// We pass our two functions here, to run when our endpoint receives a request.
+app.get('/', myMiddleware, myRootRouteHandler);
 
 app.listen(port, () => console.log('Server started on port ' + port));
